3bb921fd3eb1c06870cc6d3896596bc37ab40513
DJGilmore4U <daniellejgilmore@gmail.com>
2018-04-10 16:41:42 -0700

    fixed index api pulls

--- js/news.js
+++ js/news.js
@@ -13,13 +13,17 @@ object= {};
                     var results = response.articles;
                     object = response.articles;
 					for (var i = 0; i < object.length; i++) {
-                    var news = object[i].title;
-                    console.log(news);
+					var news = object[i].title;
+					var link = object[i].url;
+					console.log(news);
+					console.log(link);
+					console.log(object);
                     }
                     
-                    document.getElementById("uno").innerHTML = "<h3> Trending Tech News </h3><span>"+ object[0].title + "</span>";
-                    document.getElementById("dos").innerHTML = "<h3> More Tech News </h3><span>"+ object[1].title + "</span>";
-                    document.getElementById("tres").innerHTML = "<h3> Even More Tech News </h3><span>"+ object[2].title + "</span>";
+                    document.getElementById("uno").innerHTML = "<h3 id= 'dup'; class='text-center'><a href='"+object[0].url+"'>"+object[0].title+"</a></h3><h5 id= 'dup'; class='text-center;'>"+object[0].description+"</h5>";
+                    document.getElementById("dos").innerHTML = "<h3 id= 'dup'; class='text-center'><a href='"+object[1].url+"'>"+object[1].title+"</a></h3><h5 id= 'dup'; class='text-center;'>"+object[1].description+"</h5>";
+					document.getElementById("tres").innerHTML = "<h3 id= 'dup'; class='text-center'><a href='"+object[2].url+"'>"+object[2].title+"</a></h3><h5 id= 'dup'; class='text-center;'>"+object[2].description+"</h5>";
+					document.getElementById("quatro").innerHTML = "<h3 id= 'dup'; class='text-center'><a href='"+object[3].url+"'>"+object[3].title+"</a></h3><h5 id= 'dup'; class='text-center;'>"+object[3].description+"</h5>";
                     
 	
 	});
