dac89b17f83f369cb611799fbc7bc460cfe8e7e6
DJGilmore4U <daniellejgilmore@gmail.com>
2018-04-10 17:17:24 -0700

    various updates

--- js/login.js
+++ js/login.js
@@ -1,16 +1,5 @@
   // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyCW6s0biYO48fItNvKaAdfn0QkUjnztHFo",
-    authDomain: "portfolio-94b41.firebaseapp.com",
-    databaseURL: "https://portfolio-94b41.firebaseio.com",
-    projectId: "portfolio-94b41",
-    storageBucket: "portfolio-94b41.appspot.com",
-    messagingSenderId: "974810211301"
- };
-src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js";
-firebase.initializeApp(config);
-var database = firebase.database();
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
+  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+
   
-  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+ var queryUrl= "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=123456789&redirect_uri=https%3A%2F%2FDJGilmore4U.github.io%2Fauth%2Flinkedin&state=987654321&scope=r_basicprofile";
